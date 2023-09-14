fbcefc8793e41bd0ced79c3eb5d188ff4a884e81
smaelkom <smael.komen@gmail.com>
2023-09-14 10:18:23 -0700

    gcc -

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,12 +6,10 @@
  * Return 1 for character that will be a digit or 0 for any else
  */
 
-int _isdigit(int x);
-
+int _isdigit(int x)
 {
 	if (x >= 48 && x <= 57)
-	{
 		return (1);
-	}
+	else
 	return (0);
 }
