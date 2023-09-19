18d9abc8b068184c733834ee89f6e8a71c8ae233
smaelkom <smael.komen@gmail.com>
2023-09-19 12:43:42 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0
+ * reset_to_98 -  function that takes a pointer to an int as parameter and updates the value it points to to 98.
+ * @n: pointer to the vlue to be changed
+ * Return: void
  */
-int main(void)
-{
-	int n;
 
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
